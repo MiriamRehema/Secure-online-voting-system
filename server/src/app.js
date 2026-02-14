f9db12655d8147505ee180987b1e19ac1a762b54
MiriamRehema <rehemamiriam30@gmail.com>
2026-02-14 20:35:06 +0200

    updated app.js and env files

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,3 @@
-//created the Express App,adds middlewares,register routes
 const express = require("express");
 const cors = require("cors");
 
@@ -8,17 +7,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Routes
+app.use("/api/students", require("./routes/students.routes"));
+app.use("/api/candidates", require("./routes/candidates.routes"));
+app.use("/api/admin", require("./routes/admin.routes"));
+app.use("/api/votes", require("./routes/vote.routes"));
+app.use("/api/auth", require("./routes/auth.routes"));
 
-// Import routes
-const candidateRoutes = require("./routes/candidates.route");
-
-// Use routes
-app.use("/api/candidates", candidateRoutes);
-
-// Test route (VERY IMPORTANT)
+// Test route
 app.get("/", (req, res) => {
-  res.json({
-    message: "Secure Online Voting System API is running",
-  });
+  res.json({ message: "Secure Online Voting System API is running" });
 });
+
 module.exports = app;
