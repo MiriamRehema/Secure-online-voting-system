226ba070ae0f4a65854ab240d1de48fee4585f4d
MiriamRehema <rehemamiriam30@gmail.com>
2026-02-17 20:18:54 +0200

    changes server.js for testing

--- server/server.js
+++ server/server.js
@@ -1,12 +1,19 @@
-require("dotenv").config();
+const dotenv = require("dotenv");
+const path = require("path");
+dotenv.config({
+  path: process.env.NODE_ENV === "test" 
+    ? path.resolve(__dirname, "../.env.test") 
+    : path.resolve(__dirname, "../.env"),
+});
+
 const connectDB = require("./src/config/db");
 const app = require("./src/app");
 
-const PORT = process.env.PORT || 5000;
-
-// Connect to MongoDB
-connectDB();
-
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
-});
+const mongoUri = process.env.MONGO_URI;
+connectDB(mongoUri)
+  .then(() => {
+    console.log("MongoDB connected");
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch(err => console.error(err));
