a915a73084f1ff80093f16d0baae4620e6320f2b
MiriamRehema <rehemamiriam30@gmail.com>
2026-04-09 19:10:36 +0200

    added all the api routes

--- server/src/app.js
+++ server/src/app.js
@@ -3,11 +3,13 @@ const cors = require("cors");
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
+
+
 // Routes
+app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/students", require("./routes/students.route"));
 app.use("/api/candidates", require("./routes/candidates.route"));
 app.use("/api/admin", require("./routes/admin.routes"));
