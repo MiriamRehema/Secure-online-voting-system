3d95189efe1a8f876d1f53581f0904659db02517
Miriam Rehema <135101807+MiriamRehema@users.noreply.github.com>
2026-04-09 18:14:25 +0200

    Delete Frontend/secure-online-voting-system directory
    deleted these files to replace them with something new

--- Frontend/secure-online-voting-system/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="footer">
-      <p>© {new Date().getFullYear()} Jomo Kenyatta University of Agriculture and Technology. All Rights Reserved.</p>
-    </footer>
-  );
-};
-
-export default Footer;
