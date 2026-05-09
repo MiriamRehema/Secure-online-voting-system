7af1d90ca46a20dcaa7de1cce337c1596e9d4fe9
MiriamRehema <rehemamiriam30@gmail.com>
2026-05-09 18:50:27 +0200

    corrected app.js to fit in cors

--- server/src/app.js
+++ server/src/app.js
@@ -1,16 +1,17 @@
 const express = require("express");
-
-
+const cors = require("cors");
 const app = express();
 
-const cors = require("cors");
-
-app.use(cors({
+const corsOptions = {
   origin: "http://localhost:3000",
-  methods: ["GET", "POST", "PUT", "DELETE","OPTIONS"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
-  
-}));
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
+
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -18,16 +19,12 @@ app.use((req, res, next) => {
   next();
 });
 
-// Routes
 app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/student", require("./routes/students.route"));
 app.use("/api/candidates", require("./routes/candidates.route"));
 app.use("/api/admin", require("./routes/admin.routes"));
 app.use("/api/vote", require("./routes/vote.routes"));
 
-
-
-// Test route
 app.get("/", (req, res) => {
   res.json({ message: "Secure Online Voting System API is running" });
 });
