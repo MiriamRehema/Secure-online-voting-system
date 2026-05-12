11b1dd2ec71d7afd2edec16af7d24995120b363f
MiriamRehema <rehemamiriam30@gmail.com>
2026-05-12 17:16:45 +0200

    password hashing added

--- server/src/models/Student.js
+++ server/src/models/Student.js
@@ -1,23 +1,30 @@
 const mongoose = require("mongoose");
-
+const bcrypt = require("bcryptjs");
 const studentSchema = new mongoose.Schema({
   regNumber: {
     type: String,
     required: true,
     unique: true,
+    trim:true,
+    lowercase:true,
+    
   },
   password: {
     type: String,
     required: true,
+    minlength:6,
   },
   fullName: {
     type: String,
     required: true,
+    trim:true,
   },
   email: {
     type: String,
     required: true,
     unique: true,
+    lowercase:true,
+    trim:true,
   },
   course: {
     type: String,
@@ -43,5 +50,16 @@ const studentSchema = new mongoose.Schema({
    { type: String, 
     default: null },
 });
+studentSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+
+  this.password = await bcrypt.hash(this.password, 10);
+  
+});
+// 🔑 PASSWORD COMPARE METHOD
+studentSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
 
 module.exports = mongoose.model("Student", studentSchema);
