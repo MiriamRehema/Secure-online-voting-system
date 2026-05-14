e9c0f1af385b2886460b319499f416fe530b7836
Marion Tamnai <mariontamnai@gmail.com>
2026-05-14 17:38:06 +0300

    Refactor Student model for code consistency

--- server/src/models/Student.js
+++ server/src/models/Student.js
@@ -5,26 +5,25 @@ const studentSchema = new mongoose.Schema({
     type: String,
     required: true,
     unique: true,
-    trim:true,
-    lowercase:true,
-    
+    trim: true,
+    lowercase: true,
   },
   password: {
     type: String,
     required: true,
-    minlength:6,
+    minlength: 6,
   },
   fullName: {
     type: String,
     required: true,
-    trim:true,
+    trim: true,
   },
   email: {
     type: String,
     required: true,
     unique: true,
-    lowercase:true,
-    trim:true,
+    lowercase: true,
+    trim: true,
   },
   course: {
     type: String,
@@ -42,24 +41,24 @@ const studentSchema = new mongoose.Schema({
     type: Boolean,
     default: false,
   },
+  isFirstLogin: {
+    type: Boolean,
+    default: true,
+  },
   createdAt: {
     type: Date,
     default: Date.now,
   },
-   votingToken: 
-   { type: String, 
-    default: null },
+  votingToken: {
+    type: String,
+    default: null
+  },
 });
 studentSchema.pre("save", async function () {
   if (!this.isModified("password")) return;
-
   this.password = await bcrypt.hash(this.password, 10);
-  
 });
-// 🔑 PASSWORD COMPARE METHOD
 studentSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
-
-
 module.exports = mongoose.model("Student", studentSchema);
