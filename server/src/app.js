70828eb013a9d8eca3ce657337981ee645f62655
Marion Tamnai <mariontamnai@gmail.com>
2026-05-15 12:21:21 +0300

    Add 'x-voting-token' to allowed CORS headers

--- server/src/app.js
+++ server/src/app.js
@@ -15,7 +15,7 @@ const corsOptions = {
     }
   },
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  allowedHeaders: ["Content-Type", "Authorization", "x-voting-token"],
   credentials: true,
   optionsSuccessStatus: 200
 };
